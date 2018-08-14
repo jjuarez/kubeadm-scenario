313d87052eab2235fcd71a687c247a6259ade0d4
Javier Juarez <javier.juarez@fon.com>
2018-08-14 18:12:06 +0200

    Solves the provision for a Debian Stretch

--- provision/shell/install_base.sh
+++ provision/shell/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update &&
-apt-get install -qy ca-certificates curl software-properties-common apt-transport-https
+apt-get install -qy ca-certificates curl software-properties-common apt-transport-https gnupg2
--- provision/shell/install_docker.sh
+++ provision/shell/install_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu/
+"deb [arch=amd64] https://download.docker.com/linux/debian/
 $(lsb_release -cs)
 stable"
+
 apt-get update &&
 apt-get install -qy docker-ce
